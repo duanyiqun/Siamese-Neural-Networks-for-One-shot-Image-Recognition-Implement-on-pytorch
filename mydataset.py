c3d4ec95f509cdf51c86b474379e17978ddde0c0
Claude Duan <claude.duan@2hatsecurity.com>
2018-12-19 15:02:47 -0800

    init

---
+++ mydataset.py
@@ -0,0 +1,131 @@
+import torch
+from torch.utils.data import Dataset, DataLoader
+import os
+from numpy.random import choice as npc
+import numpy as np
+import time
+import random
+import torchvision.datasets as dset
+from PIL import Image
+import torchvision.transform as transforms
+
+
+class OmniTrain(Dataset):
+
+    def __init__(self, dataPath, transform=transforms.ToTensor()):
+        super(OmniTrain, self).__init__()
+        np.random.seed(0)
+        self.transform = transform
+        self.imglist, self.num_classes = self.build_index(dataPath)
+
+    def img_loader(self, path):
+        degrees  = [0, 90, 180, 270]
+        return Image.open(path).rotate(random.choice(degrees)).convert('L')
+
+    def build_index(self, dataPath):
+        print("Establish image indeces for training...")
+        datas = {}
+        idx = 0
+        for alphaPath in os.listdir(dataPath):
+            for charPath in os.listdir(os.path.join(dataPath, alphaPath)):
+                datas[idx] = []
+                for samplePath in os.listdir(os.path.join(dataPath, alphaPath, charPath)):
+                    filePath = os.path.join(dataPath, alphaPath, charPath, samplePath)
+                        #datas[idx].append(Image.open(filePath).rotate(agree).convert('L'))
+                    datas[idx].append(filePath)
+                idx += 1
+        print("finish creating training dataset ... there are totally {} classes".format(idx))
+        return datas, idx
+
+    def __len__(self):
+        return  len(self.imglist)
+
+    def __getitem__(self, index):
+        # image1 = random.choice(self.dataset.imgs)
+        label = None
+        image1 = None
+        image2 = None
+        # get image from same class
+        if index % 2 == 1:
+            label = 1.0
+            idx1 = random.randint(0, self.num_classes - 1)
+            image1 = self.img_loader(random.choice(self.imglist[idx1]))
+            image2 = self.img_loader(random.choice(self.imglist[idx1]))
+        # get image from different class
+        else:
+            label = 0.0
+            idx1 = random.randint(0, self.num_classes - 1)
+            idx2 = random.randint(0, self.num_classes - 1)
+            while idx1 == idx2:
+                idx2 = random.randint(0, self.num_classes - 1)
+            image1 = self.img_loader(random.choice(self.imglist[idx1]))
+            image2 = self.img_loader(random.choice(self.imglist[idx2]))
+
+        if self.transform:
+            image1 = self.transform(image1)
+            image2 = self.transform(image2)
+        return image1, image2, torch.from_numpy(np.array([label], dtype=np.float32))
+
+
+class OmniTest(Dataset):
+
+    def __init__(self, dataPath, transform=transforms.ToTensor(), times=200, way=20):
+        np.random.seed(1)
+        super(OmniTest, self).__init__()
+        self.transform = transform
+        self.times = times
+        self.way = way
+        self.img1 = None
+        self.c1 = None
+        self.datas, self.num_classes = self.loadToMem(dataPath)
+    
+    def img_loader(self, path):
+        return Image.open(path).convert('L')
+
+    def loadToMem(self, dataPath):
+        print("Establish image indeces to testing...")
+        datas = {}
+        idx = 0
+        for alphaPath in os.listdir(dataPath):
+            for charPath in os.listdir(os.path.join(dataPath, alphaPath)):
+                datas[idx] = []
+                for samplePath in os.listdir(os.path.join(dataPath, alphaPath, charPath)):
+                    filePath = os.path.join(dataPath, alphaPath, charPath, samplePath)
+                        #datas[idx].append(Image.open(filePath).rotate(agree).convert('L'))
+                    datas[idx].append(filePath)
+                idx += 1
+        print("finish creating test dataset ... there are totally {} classes".format(idx))
+        return datas, idx
+
+    def __len__(self):
+        return self.times * self.way
+
+    def __getitem__(self, index):
+        idx = index % self.way
+        label = None
+        # generate image pair from same class
+        if idx == 0:
+            self.c1 = random.randint(0, self.num_classes - 1)
+            self.img1 = self.img_loader(random.choice(self.datas[self.c1]))
+            img2 = self.img_loader(random.choice(self.datas[self.c1]))
+            label = 1.0
+        # generate image pair from different class
+        else:
+            c2 = random.randint(0, self.num_classes - 1)
+            while self.c1 == c2:
+                c2 = random.randint(0, self.num_classes - 1)
+            img2 = self.img_loader(random.choice(self.datas[c2]))
+            label = 0.0
+
+        if self.transform:
+            img1 = self.transform(self.img1)
+            img2 = self.transform(img2)
+        return img1, img2, torch.from_numpy(np.array([label], dtype=np.float32))
+
+
+# test
+if __name__=='__main__':
+    trainSet = OmniTrain('./images_background',transform=transforms.ToTensor())
+    trainLoader = DataLoader(trainSet, batch_size=2, shuffle=False, num_workers=2)
+    for batch_id, (img1, img2, label) in enumerate(trainLoader, 1):
+        print(batch_id, img1, img2, label)
---
+++ train.py
@@ -0,0 +1,123 @@
+import torch
+import pickle
+import torchvision
+from torchvision import transforms
+import torchvision.datasets as dset
+from torchvision import transforms
+from mydataset import OmniTrain, OmniTest
+from torch.utils.data import DataLoader
+from torch.autograd import Variable
+import matplotlib.pyplot as plt
+from model import Siamese
+import time
+import numpy as np
+import gflags
+import sys
+from collections import deque
+import os
+
+
+if __name__ == '__main__':
+
+    Flags = gflags.FLAGS
+    gflags.DEFINE_bool("cuda", True, "use cuda")
+    gflags.DEFINE_string("train_path", "/home/data/pin/data/omniglot/images_background", "training folder")
+    gflags.DEFINE_string("test_path", "/home/data/pin/data/omniglot/images_evaluation", 'path of testing folder')
+    gflags.DEFINE_integer("way", 20, "how much way one-shot learning")
+    gflags.DEFINE_string("times", 400, "number of samples to test accuracy")
+    gflags.DEFINE_integer("workers", 4, "number of dataLoader workers")
+    gflags.DEFINE_integer("batch_size", 128, "number of batch size")
+    gflags.DEFINE_float("lr", 0.00006, "learning rate")
+    gflags.DEFINE_integer("show_every", 10, "show result after each show_every iter.")
+    gflags.DEFINE_integer("save_every", 100, "save model after each save_every iter.")
+    gflags.DEFINE_integer("test_every", 100, "test model after each test_every iter.")
+    gflags.DEFINE_integer("max_iter", 50000, "number of iterations before stopping")
+    gflags.DEFINE_string("model_path", "/home/data/pin/model/siamese", "path to store model")
+    gflags.DEFINE_string("gpu_ids", "0,1,2,3", "gpu ids used to train")
+
+    Flags(sys.argv)
+
+    data_transforms = transforms.Compose([
+        transforms.RandomAffine(15),
+        transforms.ToTensor()
+    ])
+
+
+    # train_dataset = dset.ImageFolder(root=Flags.train_path)
+    # test_dataset = dset.ImageFolder(root=Flags.test_path)
+
+
+    os.environ["CUDA_VISIBLE_DEVICES"] = Flags.gpu_ids
+    print("use gpu:", Flags.gpu_ids, "to train.")
+
+    trainSet = OmniglotTrain(Flags.train_path, transform=data_transforms)
+    testSet = OmniglotTest(Flags.test_path, transform=transforms.ToTensor(), times = Flags.times, way = Flags.way)
+    testLoader = DataLoader(testSet, batch_size=Flags.way, shuffle=False, num_workers=Flags.workers)
+
+    trainLoader = DataLoader(trainSet, batch_size=Flags.batch_size, shuffle=False, num_workers=Flags.workers)
+
+    loss_fn = torch.nn.BCEWithLogitsLoss(size_average=True)
+    net = Siamese()
+
+    # multi gpu
+    if len(Flags.gpu_ids.split(",")) > 1:
+        net = torch.nn.DataParallel(net)
+
+    if Flags.cuda:
+        net.cuda()
+
+    net.train()
+
+    optimizer = torch.optim.Adam(net.parameters(),lr = Flags.lr )
+    optimizer.zero_grad()
+
+    train_loss = []
+    loss_val = 0
+    time_start = time.time()
+    queue = deque(maxlen=20)
+
+    for batch_id, (img1, img2, label) in enumerate(trainLoader, 1):
+        if batch_id > Flags.max_iter:
+            break
+        if Flags.cuda:
+            img1, img2, label = Variable(img1.cuda()), Variable(img2.cuda()), Variable(label.cuda())
+        else:
+            img1, img2, label = Variable(img1), Variable(img2), Variable(label)
+        optimizer.zero_grad()
+        output = net.forward(img1, img2)
+        loss = loss_fn(output, label)
+        loss_val += loss.data[0]
+        loss.backward()
+        optimizer.step()
+        if batch_id % Flags.show_every == 0 :
+            print('[%d]\tloss:\t%.5f\tTook\t%.2f s'%(batch_id, loss_val/Flags.show_every, time.time() - time_start))
+            loss_val = 0
+            time_start = time.time()
+        if batch_id % Flags.save_every == 0:
+            torch.save(net.state_dict(), Flags.model_path + '/model-inter-' + str(batch_id+1))
+        if batch_id % Flags.test_every == 0:
+            right, error = 0, 0
+            for _, (test1, test2) in enumerate(testLoader, 1):
+                if Flags.cuda:
+                    test1, test2 = test1.cuda(), test2.cuda()
+                test1, test2 = Variable(test1), Variable(test2)
+                output = net.forward(test1, test2).data.cpu().numpy()
+                pred = np.argmax(output)
+                if pred == 0:
+                    right += 1
+                else: error += 1
+            print('*'*70)
+            print('[%d]\tright:\t%d\terror:\t%d\tprecision:\t%f'%(batch_id, right, error, right*1.0/(right+error)))
+            print('*'*70)
+            queue.append(right*1.0/(right+error))
+        train_loss.append(loss_val)
+    #  learning_rate = learning_rate * 0.95
+
+    with open('train_loss', 'wb') as f:
+        pickle.dump(train_loss, f)
+
+    acc = 0.0
+    for d in queue:
+        acc += d
+    print("#"*70)
+    print("final accuracy: ", acc/20)
